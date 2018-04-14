1decae9aecadaa2dc76c421ae4643dd494bb35f8
ethancrist <root@usloft3621.serverprofi24.com>
2018-04-14 13:09:39 -0500

    Update all of master

---
+++ core.js
@@ -0,0 +1,15 @@
+/**
+ * @title Webcore
+ * @description Lightweight, feature-rich, all purpose library for the web.
+ * @author ethancrist
+ * @git https://github.com/ethancrist/webcore.git
+ **/
+
+var Core = {
+    init: function(options) {
+        console.log('Webcore initialized');
+    },
+    setOptions: function(defaultOptions, options) {
+        console.log('set dem options')
+    }
+}
